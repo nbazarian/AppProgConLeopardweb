abaf85475a03c3e875e9f87e727507163b54950d
nbazarian <84804518+nbazarian@users.noreply.github.com>
2021-07-02 20:08:13 -0400

    Update admin.h

--- admin.h
+++ admin.h
@@ -1,3 +1,4 @@
+//admin file written by Nuno Bazarian
 #ifndef ADMIN_H
 #define ADMIN_H
 
@@ -43,6 +44,7 @@ class admin_c: public user_c{
                 	std::cout << "Records deleted Successfully!" << std::endl;
 
         }
+	//Placeholder functions for final product (I am lazy and don't want to rewrite them for this assignment)
         void modUSystem(){ //add/remove user from system
             std::cout << "User removed. \n";
         }
@@ -50,7 +52,7 @@ class admin_c: public user_c{
         void modSCourse(){ //add/remove student from course
             std::cout << "Student removed from course.\n";
         }
-
+	//this function differs because the Admin will be able to see personal data about users, not just information about courses
         void searchData(){ //function to search for data in the system and print it
             std::cout << "Information found and displayed from system.\n";
         }
