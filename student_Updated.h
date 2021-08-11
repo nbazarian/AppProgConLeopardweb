a3e6409775959547d58e4794682ff45a32c37bfa
patelb3 <68629166+patelb3@users.noreply.github.com>
2021-08-11 17:51:24 -0400

    Update student_Updated.h

--- student_Updated.h
+++ student_Updated.h
@@ -4,19 +4,19 @@
 
 #include "user.h"
 using namespace std;
-class student_c: public user_c{
-    public:
-        void printAll() { //print admin's info
-            print();
-        }
+class student_c : public user_c {
+public:
+	void printAll() { //print admin's info
+		print();
+	}
 
-        student_c(std::string fName, std::string lName, std::string inD):user_c(fName, lName, inD){}
+	student_c(std::string fName, std::string lName, std::string inD) :user_c(fName, lName, inD) {}
 
- 
- //Add or remove from course schedule 
+
+	//Add or remove from course schedule 
 	void addToSchedule(sqlite3* DB, string ID) {
 		int another, error, numCourses = -1, already, count;
-		std::string courseToAdd[5] = {"NULL", "NULL", "NULL", "NULL", "NULL"};
+		std::string courseToAdd[5] = { "NULL", "NULL", "NULL", "NULL", "NULL" };
 		vector<string> registeredCourses;
 
 		//Display all available courses 
@@ -60,17 +60,17 @@ class student_c: public user_c{
 		}
 
 		//Update database to add courses 
-		string addCourses = "UPDATE STUDENT SET COURSE1 = '" + courseToAdd[0] + "', COURSE2 = '" + courseToAdd[1] + "', COURSE3 = '" + courseToAdd[2] + "', COURSE4 = '" + courseToAdd[3] +                         "', COURSE5 = '" + courseToAdd[4] + "' WHERE ID = '" + ID + "'";
+		string addCourses = "UPDATE STUDENT SET COURSE1 = '" + courseToAdd[0] + "', COURSE2 = '" + courseToAdd[1] + "', COURSE3 = '" + courseToAdd[2] + "', COURSE4 = '" + courseToAdd[3] + "', COURSE5 = '" + courseToAdd[4] + "' WHERE ID = '" + ID + "'";
 		rfc = sqlite3_exec(DB, addCourses.c_str(), callback, NULL, NULL);
-		
+
 		//Print Student's schedule
 		cout << "\n**********\n SCHEDULE\n**********\n";
 		string printCourses = "SELECT * FROM STUDENT WHERE ID = '" + ID + "';";
 		rfc = sqlite3_exec(DB, printCourses.c_str(), callback, NULL, NULL);
 	}
- 
- 
- //Remove course from schedule 
+
+
+	//Remove course from schedule 
 	void removeFromSchedule(sqlite3* DB, string ID) {
 		int courseToRemove, another = 1;
 		string removeCourse;
@@ -80,41 +80,41 @@ class student_c: public user_c{
 		while (another == 1) {
 			string printCourses = "SELECT NAME, COURSE1, COURSE2, COURSE3, COURSE4, COURSE5 FROM STUDENT WHERE ID = '" + ID + "';";
 			rfc = sqlite3_exec(DB, printCourses.c_str(), callback, NULL, NULL);
-			
+
 			cout << "Which course would you like to remove?(1,2,3,4,5): ";
 			cin >> courseToRemove;
 
 			//Update database to remove desired course(s)
 			switch (courseToRemove) {
-				case 1:
-					removeCourse = "UPDATE STUDENT SET COURSE1 = NULL WHERE ID = '" + ID + "'";
-					rfc = sqlite3_exec(DB, removeCourse.c_str(), callback, NULL, NULL);
+			case 1:
+				removeCourse = "UPDATE STUDENT SET COURSE1 = NULL WHERE ID = '" + ID + "'";
+				rfc = sqlite3_exec(DB, removeCourse.c_str(), callback, NULL, NULL);
 				break;
-				case 2:
-					removeCourse = "UPDATE STUDENT SET COURSE2 = NULL WHERE ID = '" + ID + "'";
-					rfc = sqlite3_exec(DB, removeCourse.c_str(), callback, NULL, NULL);
+			case 2:
+				removeCourse = "UPDATE STUDENT SET COURSE2 = NULL WHERE ID = '" + ID + "'";
+				rfc = sqlite3_exec(DB, removeCourse.c_str(), callback, NULL, NULL);
 				break;
-				case 3:
-					removeCourse = "UPDATE STUDENT SET COURSE3 = NULL WHERE ID = '" + ID + "'";
-					rfc = sqlite3_exec(DB, removeCourse.c_str(), callback, NULL, NULL);
+			case 3:
+				removeCourse = "UPDATE STUDENT SET COURSE3 = NULL WHERE ID = '" + ID + "'";
+				rfc = sqlite3_exec(DB, removeCourse.c_str(), callback, NULL, NULL);
 				break;
-				case 4:
-					removeCourse = "UPDATE STUDENT SET COURSE4 = NULL WHERE ID = '" + ID + "'";
-					rfc = sqlite3_exec(DB, removeCourse.c_str(), callback, NULL, NULL);
+			case 4:
+				removeCourse = "UPDATE STUDENT SET COURSE4 = NULL WHERE ID = '" + ID + "'";
+				rfc = sqlite3_exec(DB, removeCourse.c_str(), callback, NULL, NULL);
 				break;
-				case 5:
-					removeCourse = "UPDATE STUDENT SET COURSE5 = NULL WHERE ID = '" + ID + "'";
-					rfc = sqlite3_exec(DB, removeCourse.c_str(), callback, NULL, NULL);
+			case 5:
+				removeCourse = "UPDATE STUDENT SET COURSE5 = NULL WHERE ID = '" + ID + "'";
+				rfc = sqlite3_exec(DB, removeCourse.c_str(), callback, NULL, NULL);
 				break;
-				default: 
-					"ERROR: Invalid\n";
+			default:
+				"ERROR: Invalid\n";
 				break;
 
 			}
 
 			cout << "\n\nRemove another Course? (1 - Yes, 0 - No): ";
 			cin >> another;
-		} 
+		}
 
 		//Print out updated student schedule 
 		cout << "\n**********\n SCHEDULE\n**********\n";
